cbc9411dd6b235d512bdcbca0ff671e7df967020
henk687 <henkleenheer@upcmail.nl>
2021-12-08 00:26:19 +0100

    first commit

---
+++ js/api.js
@@ -0,0 +1,94 @@
+const movieSearchBox = document.getElementById('movie-search-box');
+const searchList = document.getElementById('search-list');
+const resultGrid = document.getElementById('result-grid');
+
+
+// load movies from API
+const loadMovies = async (searchTerm) => {
+  const URL = `http://www.omdbapi.com/?s=${searchTerm}&apikey=38313949`;
+  const res = await fetch(`${URL}`);
+  const data = await res.json();
+  if(data.Response == "True") displayMovieList(data.Search);
+
+}
+
+const findMovies = () => {
+  let searchTerm = (movieSearchBox.value).trim();
+  if(searchTerm.length > 0) {
+    searchList.classList.remove('hide-search-list');
+    loadMovies(searchTerm);
+  } else {
+    searchList.classList.add('hide-search-list');
+  }
+}
+
+const displayMovieList = (movies) => {
+  searchList.innerHTML = "";
+  for(let idx = 0; idx < movies.length; idx++) {
+    let movieListItem = document.createElement('div')
+    // setting movie id in data id
+    movieListItem.dataset.id = movies[idx].imdbID; 
+    movieListItem.classList.add('search-list-item');
+    if(movies[idx].Poster != "N/A")
+      moviePoster = movies[idx].Poster;
+    else
+      moviePoster= "../images/home-shape.png"
+
+    movieListItem.innerHTML = `
+      <div class="search-item-thumbnail">
+        <img src="${moviePoster}" alt="">
+      </div>
+      <div class="search-item-info">
+        <h3>${movies[idx].Title}</h3>
+        <p>${movies[idx].Year}</p>
+      </div>
+    `;
+    searchList.appendChild(movieListItem);
+  }
+  loadMovieDetails();
+}
+
+const loadMovieDetails = () => {
+  const searchListMovies = searchList.querySelectorAll('.search-list-item');
+  searchListMovies.forEach(movie => {
+    movie.addEventListener('click', async () => {
+      // console.log(movie.dataset.id)
+      searchList.classList.add('hide-search-list');
+      movieSearchBox.value = "";
+      const result = await fetch(`http://www.omdbapi.com/?i=${movie.dataset.id}&apikey=38313949`)
+      const movieDetails = await result.json();
+      // console.log(movieDetails);
+      displayMovieDetails(movieDetails);
+    })
+  });
+}
+
+const displayMovieDetails = (details) => {
+  resultGrid.innerHTML = `
+    <div class = "movie-poster">
+      <img src = "${(details.Poster != "N/A") ? details.Poster : "image_not_found.png"}" alt = "movie poster">
+    </div>
+    <div class="movie-info">
+      <h3 class="movie-title">
+        ${details.Title}
+      </h3>
+      <ul class="movie-misc-info">
+        <li class="year">Year: ${details.Year}</li>
+        <li class="rated">Ratings: ${details.Rated}</li>
+        <li class="released">Released: ${details.Released}</li>
+      </ul>
+      <p class = "genre"><b>Genre:</b> ${details.Genre}</p>
+      <p class = "writer"><b>Writer:</b> ${details.Writer}</p>
+      <p class = "actors"><b>Actors: </b>${details.Actors}</p>
+      <p class = "plot"><b>Plot:</b> ${details.Plot}</p>
+      <p class = "language"><b>Language:</b> ${details.Language}</p>
+      <p class = "awards"><b><i class = "fas fa-award"></i></b> ${details.Awards}</p>
+    </div>
+  `;
+}
+
+window.addEventListener('click', (event) => {
+  if(event.target.className != "form-control"){
+      searchList.classList.add('hide-search-list');
+  }
+});
---
+++ js/script.js
@@ -0,0 +1,61 @@
+// mobile menu
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector(".header .navbar");
+const header = document.querySelector('.header');
+const navLinks = document.querySelectorAll('.navlink');
+
+menu.onclick = () => {
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+}
+
+scrollingElement = (document.scrollingElement || document.body)
+const scrollToBottom = () => {
+  scrollingElement.scrollTop = scrollingElement.scrollHeight;
+}
+
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#fff';
+    navLinks.forEach((link) => {
+      link.style.color = '#000'
+    })
+	} else {
+		header.style.backgroundColor = 'transparent';
+    navLinks.forEach((link) => {
+      link.style.color = '#fff'
+    })
+	}
+});
+
+
+// reviews
+let slides = document.querySelectorAll('.reviews .slide-container .slide');
+let index = 0;
+
+const next = () => {
+  slides[index].classList.remove('active');
+  index = (index + 1) % slides.length;
+  slides[index].classList.add('active')
+}
+
+const prev = () => {
+  slides[index].classList.remove('active');
+  index = (index - 1 + slides.length) % slides.length;
+  slides[index].classList.add('active');
+}
+
+//faq
+let faqAccordion = document.querySelectorAll('.faq .accordion-container .accordion')
+
+faqAccordion.forEach(accordion => {
+  accordion.onclick = () => {
+    accordion.classList.toggle('active')
+  }
+})
