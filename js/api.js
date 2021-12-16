aa68351d6a5729e0456eb86e7b58bb1c5d94c9b3
henk687 <henkleenheer@upcmail.nl>
2021-12-16 19:50:04 +0100

    changes

--- js/api.js
+++ js/api.js
@@ -5,7 +5,7 @@ const resultGrid = document.getElementById('result-grid');
 
 // load movies from API
 const loadMovies = async (searchTerm) => {
-  const URL = `http://www.omdbapi.com/?s=${searchTerm}&apikey=38313949`;
+  const URL = `https://www.omdbapi.com/?s=${searchTerm}&apikey=38313949`;
   const res = await fetch(`${URL}`);
   const data = await res.json();
   if(data.Response == "True") displayMovieList(data.Search);
@@ -55,7 +55,7 @@ const loadMovieDetails = () => {
       // console.log(movie.dataset.id)
       searchList.classList.add('hide-search-list');
       movieSearchBox.value = "";
-      const result = await fetch(`http://www.omdbapi.com/?i=${movie.dataset.id}&apikey=38313949`)
+      const result = await fetch(`https://www.omdbapi.com/?i=${movie.dataset.id}&apikey=38313949`)
       const movieDetails = await result.json();
       // console.log(movieDetails);
       displayMovieDetails(movieDetails);
