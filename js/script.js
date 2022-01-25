8ca42487c5db37141d06bbc22307f426d9db3036
henk687 <henkleenheer@upcmail.nl>
2022-01-25 14:41:30 +0100

    changes

--- js/script.js
+++ js/script.js
@@ -9,10 +9,10 @@ menu.onclick = () => {
   navbar.classList.toggle('active');
 }
 
-window.onscroll = () => {
-  menu.classList.remove('fa-times');
-  navbar.classList.remove('active');
-}
+// window.onscroll = () => {
+//   menu.classList.remove('fa-times');
+//   navbar.classList.remove('active');
+// }
 
 // scrollingElement = (document.scrollingElement || document.body)
 // const scrollToBottom = () => {
